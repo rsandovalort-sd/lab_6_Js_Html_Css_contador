2c96dc368d8e43dbfb5bec46ba438ebc3ca16312
ricardoSo-sd <rsandovalort@gmail.com>
2025-11-06 17:53:11 -0500

    Agregada función contar hacia atrás

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+const numero = document.getElementById("contador");
+let contador = 1
+const limite_max = 20;
+const limite_min = 1;
+
 document.getElementById("aumentar").addEventListener('click', function(){
-    let contador = 1
-    const limite = 20;
-    const numero = document.getElementById("contador");
-
-
     const intervalo = setInterval(function(){
         numero.textContent = contador;
         if(contador >= 10) {
-            numero.style.color = "black";
-        }else{
             numero.style.color = "red";
         }
         contador++;
-        if(contador > limite){
+        if(contador > limite_max){
             clearInterval(intervalo);
         }
-
     }, 1000);
+});
 
+document.getElementById("disminuir").addEventListener('click', function(){
+    const intervalo = setInterval(function(){
+        numero.textContent = contador;
+        if (contador > limite_min){
+            contador--;
+        }   
+    }, 1000); 
 });
