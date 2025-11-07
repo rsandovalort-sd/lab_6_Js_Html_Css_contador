feb67cc0d812599689699ba243508e496c0b2729
ricardoSo-sd <rsandovalort@gmail.com>
2025-11-06 19:31:53 -0500

    Arreglos CSS

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 const numero = document.getElementById("contador");
-let contador = 1
+let contador = 0
 const limite_max = 20;
-const limite_min = 1;
+const limite_min = 0;
 
 document.getElementById("aumentar").addEventListener('click', function(){
     const intervalo = setInterval(function(){
         numero.textContent = contador;
         if(contador >= 10) {
-            numero.style.color = "red";
+            numero.style.color = "blue";
         }
         contador++;
         if(contador > limite_max){
             clearInterval(intervalo);
         }
-    }, 1000);
+    }, 500);
 });
 
 document.getElementById("disminuir").addEventListener('click', function(){
     const intervalo = setInterval(function(){
         numero.textContent = contador;
         if (contador > limite_min){
-            contador--;
+            if (contador > 0){
+                contador--;
+            }
         }   
-    }, 1000); 
+    }, 500); 
 });
